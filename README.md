8755fb4c6ba918b9bfe8e7bd253f86cf17df291b
sakthiveloo7 <sakthivelselvaraj68@gmail.com>
2024-04-23 15:25:56 +0530

    To-Do List

---
+++ README.md
@@ -0,0 +1,60 @@
+# TO-DO LIST
+
+A simple To-Do List Task is built using React and Bootstrap for a Dynamic Responsive Webpage.
+
+
+## Table of Contents
+
+- [TO-DO LIST](to-do-list)
+  - [Table of Contents](#table-of-contents)
+  - [Features](#features)
+  - [Programming Languages Used](#programming-languages-used)
+  - [Technologies Used](#technologies-used)
+  - [Nelify Deployed URL](#nelify-deployed-url)
+
+
+## Features
+
+- Add the task with a name and a description.
+- Mark tasks as completed or pending.
+- Edit the task details if needed.
+- Delete tasks that are not needed anymore from the list.
+- Responsive design using Bootstrap for a seamless experience across the devices.
+
+## Programming Languages Used
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" height="40" alt="html5 logo"  />
+  <img width="12" />
+
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" height="40" alt="css3 logo"  />
+  <img width="12" />
+
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo"  />
+  <img width="12" />
+
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" height="40" alt="bootstrap logo"  />
+  <img width="12" />
+  
+  <img src="https://skillicons.dev/icons?i=react" height="40" alt="react logo"  />
+</div>
+
+## Technologies Used
+
+<div align="left">
+  <img src="https://cdn.simpleicons.org/visualstudiocode/007ACC" height="40" alt="vscode logo"  />
+  <img width="12" />
+
+  <img src="https://cdn.simpleicons.org/git/F05032" height="40" alt="git logo"  />
+  <img width="12" />
+
+  <img src="https://cdn.simpleicons.org/github/FFFFFF" height="40" alt="git logo"  />
+  <img width="12" />
+
+  <img src="https://cdn.simpleicons.org/netlify/00C7B7" height="40" alt="netlify logo"  />
+</div>
+
+
+## Nelify Deployed URL
+
+🔸*Check out My To-Do List Here*👉🏻 [Netlify](/)
---
+++ src/components/Todo.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "../components/Todo.css";
+
+export default function Todo({ todo, updateStatus, editTodo, deleteTodo }) {
+  return (
+    <>
+      {/* Card */}
+
+      <div className="container d-flex justify-content-center align-items-center ">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div key={todo.id} className="col">
+            <div
+              class="card h-100 rounded-4 border-4"
+              style={{
+                width: "20rem",
+                background: "radial-gradient(white,#ffd89b)",
+              }}
+            >
+              <div class="card-body m-2">
+                <p class=" card-text text-black fw-bold">
+                  <span id="name">Name:</span> {todo.taskName}{" "}
+                </p>
+
+                <p className="fw-bold">
+                  <span id="description">Description:</span> {todo.description}
+                </p>
+
+                {/* Status */}
+
+                <div className="mb-2">
+                  {" "}
+                  <label htmlFor="status" className="me-1">
+                    <b id="status">Status:</b>
+                  </label>
+                  <select
+                    className={
+                      todo.status === "Completed"
+                        ? "bg-success text-white"
+                        : "bg-danger text-white"
+                    }
+                    style={{
+                      borderRadius: "5px",
+                      border: "none",
+                      //background: "#FF69B4",
+                    }}
+                    value={todo.status}
+                    name="status"
+                    onChange={(e) => updateStatus(todo.id, e.target.value)}
+                  >
+                    <option
+                      value="Completed"
+                      className={
+                        todo.status === "Completed" ? "bg-success" : ""
+                      }
+                    >
+                      Completed
+                    </option>
+                    <option
+                      value="Not Completed"
+                      className={
+                        todo.status !== "Completed"
+                          ? "bg-danger text-white"
+                          : ""
+                      }
+                    >
+                      Pending
+                    </option>
+                  </select>
+                </div>
+              </div>
+
+              {/* Edit */}
+              <div className="float-end m-3  p-2">
+                <button
+                  className="btn btn-primary mx-2"
+                  onClick={() => editTodo(todo.id)}
+                  type="button"
+                >
+                  Edit
+                </button>
+
+                {/* Delete */}
+                <button
+                  className="btn btn-danger "
+                  onClick={() => deleteTodo(todo.id)}
+                  type="button"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
